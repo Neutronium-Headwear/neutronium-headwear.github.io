a9563780af77eee6f9f1ec4fe81f385529e4b941
neutronDL <david.liang@neutroniumheadwear.com>
2024-04-27 19:16:32 -0400

    add routings
    - add category detailed page

--- assets/js/nvbar-load.js
+++ assets/js/nvbar-load.js
@@ -6,8 +6,8 @@ document.addEventListener("DOMContentLoaded", function() {
             document.getElementById('navbar-placeholder').innerHTML = html;
             // 导航HTML已经被加载和插入，现在设置active类
             var links = document.querySelectorAll('.navbar .nav-link');
-            var currentUrl = window.location.pathname.split('/').pop();  // 获取当前页面的文件名
-
+            var currentUrl = window.location.pathname.split('/');  // 获取当前页面的文件名
+            currentUrl.pop()
             links.forEach(link => {
                 if (link.getAttribute('href').includes(currentUrl)) {
                     link.classList.add('active');
